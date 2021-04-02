a53948ece689cd5a77fa400695ffcdd74ef5469c
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-04-02 06:30:45 +0600

    env applyed

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
 app.use(cors());
-app.use(express.json())
-require('dotenv').config()
-const port = process.env.PORT || 5000
-const dbuser = 'admin';
-const dbpass = 'admin123'
+app.use(express.json());
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
-
-const uri = "mongodb+srv://admin:admin123@cluster0.qde61.mongodb.net/grocery?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qde61.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     console.log('database connected')
-    const productCollection = client.db("grocery").collection("products");
-    const ordersCollection = client.db("grocery").collection("orders");
+    const productCollection = client.db(`${process.env.DB_NAME}`).collection("products");
+    const ordersCollection = client.db(`${process.env.DB_NAME}`).collection("orders");
 
     app.get('/', (req, res) => {
         res.send('Database is connected!')
@@ -92,7 +89,5 @@ client.connect(err => {
 
 
 });
-
-
 
 app.listen(port);
